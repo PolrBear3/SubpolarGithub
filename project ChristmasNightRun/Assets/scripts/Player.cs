2a36e6a34c2d3825cf4b4170895dc5188afc69ee
MYSterio3 <kdw2837@gmail.com>
2021-10-19 20:54:37 +0900

    jump

--- project ChristmasNightRun/Assets/scripts/Player.cs
+++ project ChristmasNightRun/Assets/scripts/Player.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    
     float dropRate = 2f;
     float nextDropTime = 0f;
+    
+    public float jumphight;
+    float nextJumpTime = 0f;
+    float jumpRate = 2.4f;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
     private void Update()
     {
         if (Time.time > nextDropTime)
@@ -15,6 +26,15 @@ public class Player : MonoBehaviour
             {
                 Drop();
                 nextDropTime = Time.time + 1f / dropRate;
+            }
+        }
+
+        if (Time.time > nextJumpTime)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumphight);
+                nextJumpTime = Time.time + 1f / jumpRate;
             }
         }
     }
