dfe8c1e2714683bfc7ebe65caf4cc5a3c4907a3d
MYSterio3 <kdw2837@gmail.com>
2021-10-20 19:22:06 +0900

    2110202 delete building

--- project ChristmasNightRun/Assets/scripts/Player.cs
+++ project ChristmasNightRun/Assets/scripts/Player.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D bc;
+    private Vector2 colliderSize;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+        colliderSize = bc.size;
     }
     
     private void Update()
@@ -34,6 +38,7 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        slopeCheck();
     }
 
     public Transform dropPoint;
@@ -52,5 +57,28 @@ public class Player : MonoBehaviour
     void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumphight);
+    }
+
+    [SerializeField]
+    private float slopeCheckDistance;
+
+    private void slopeCheck()
+    {
+        Vector2 checkPos = transform.position - new Vector3(0.0f, colliderSize.y / 2);
+
+        slopcheckVertical(checkPos);
+    }
+
+    private void slopCheckHorizontal(Vector2 checkPos)
+    {
+
+    }
+    private void slopcheckVertical(Vector2 checkPos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, groundLayer);
+        if (hit)
+        {
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+        }
     }
 }
