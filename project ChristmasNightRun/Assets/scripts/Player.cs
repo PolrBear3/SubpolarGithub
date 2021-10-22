5407d862c3ab5e4036b7de79384c49da3cc0ad12
MYSterio3 <kdw2837@gmail.com>
2021-10-22 21:31:29 +0900

    2110221

--- project ChristmasNightRun/Assets/scripts/Player.cs
+++ project ChristmasNightRun/Assets/scripts/Player.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private BoxCollider2D bc;
-    private Vector2 colliderSize;
+    Vector2 constForward;
+    [SerializeField] private float moveSpeed;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        bc = GetComponent<BoxCollider2D>();
-        colliderSize = bc.size;
     }
     
     private void Update()
     {
+        //Movement
+        constForward = gameObject.transform.position;
+        constForward.x += moveSpeed;
+        gameObject.transform.position = constForward;
+        
+        //Drop
         if (Time.time > nextDropTime)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -26,6 +30,7 @@ public class Player : MonoBehaviour
             }
         }
 
+        //Jump
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             if (isGrounded)
@@ -35,12 +40,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-        slopeCheck();
-    }
-
+    //Drop
     public Transform dropPoint;
     public GameObject present;
     float dropRate = 2f;
@@ -50,35 +50,12 @@ public class Player : MonoBehaviour
         Instantiate(present, dropPoint.position, dropPoint.rotation);
     }
     
+    //Jump
     public float jumphight;
     bool isGrounded;
     public Transform groundCheck;
-    public LayerMask groundLayer;
     void Jump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumphight);
-    }
-
-    [SerializeField]
-    private float slopeCheckDistance;
-
-    private void slopeCheck()
-    {
-        Vector2 checkPos = transform.position - new Vector3(0.0f, colliderSize.y / 2);
-
-        slopcheckVertical(checkPos);
-    }
-
-    private void slopCheckHorizontal(Vector2 checkPos)
-    {
-
-    }
-    private void slopcheckVertical(Vector2 checkPos)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, groundLayer);
-        if (hit)
-        {
-            Debug.DrawRay(hit.point, hit.normal, Color.green);
-        }
     }
 }
