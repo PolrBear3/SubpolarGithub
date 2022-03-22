fbd2fbf08a749a3f5c4c7992dddb4f42f5fc3c32
MYSterio3 <kdw2837@gmail.com>
2022-03-22 20:27:20 +0900

    22 03 22 01
    1. escapePod Engine CPU in progress

---
+++ project SpaceTycoon/Assets/Scripts/Ships/Escape Pod/Engine Controller/EscapePod_Engine_Controller.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EscapePod_Engine_Controller : MonoBehaviour
+{
+    // main
+    [HideInInspector]
+    public bool playerDetection;
+    public GameObject icon;
+    public GameObject mainPanel;
+
+    // sliders
+    public Slider speed;
+    public Slider energyFuel;
+    public Slider emergencyEnergyFuel;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player_hand"))
+        {
+            playerDetection = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("player_hand"))
+        {
+            playerDetection = false;
+        }
+    }
+}
