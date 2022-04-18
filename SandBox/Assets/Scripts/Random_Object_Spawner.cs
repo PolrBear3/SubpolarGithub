51ea02e1d69023bbfe4131ed529cf7d18efee560
MYSterio3 <kdw2837@gmail.com>
2022-04-18 14:50:50 +0900

    Sandbox
    changing Ui scene to game scene

--- SandBox/Assets/Scripts/Random_Object_Spawner.cs
+++ SandBox/Assets/Scripts/Random_Object_Spawner.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Random_Object_Spawner : MonoBehaviour
 {
+    private void Update()
+    {
+        SpaceKey_Input();
+    }
+
     public GameObject[] objects = new GameObject[0];
-    public Transform position;
-    GameObject randomObject;
+    public Transform[] positions = new Transform[0];
+
     int randomObjectNum;
+    int randomPositionNum;
+
+    void SpaceKey_Input()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawn_Random_Object();
+        }
+    }
 
     public void Spawn_Random_Object()
     {
         randomObjectNum = Random.Range(0, objects.Length);
-        randomObject = objects[randomObjectNum];
-        Instantiate(randomObject, position);
+        randomPositionNum = Random.Range(0, positions.Length);
+
+        Instantiate(objects[randomObjectNum], positions[randomPositionNum]);
     }
 }
