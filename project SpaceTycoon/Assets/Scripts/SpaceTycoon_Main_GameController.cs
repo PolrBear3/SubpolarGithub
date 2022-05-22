6454ba3685da17a15c3a19ef2c9c1d5af3292042
MYSterio3 <kdw2837@gmail.com>
2022-05-23 00:40:11 +0900

    .

--- project SpaceTycoon/Assets/Scripts/SpaceTycoon_Main_GameController.cs
+++ project SpaceTycoon/Assets/Scripts/SpaceTycoon_Main_GameController.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class SpaceTycoon_Main_GameController : MonoBehaviour
 {
-    private void Update()
-    {
-        Engines_Min_Set();
-    }
-
-    // UI check
+    // UI Static Bool
     public static bool isGroundOptionMenuOn = false;
     public static bool isWallOptionMenuOn = false;
     public static bool isPanelMenuOn = false;
 
-    // ship speed
+    // Object Icon Constructors
+    public void Icon_Popup_UpdateCheck(bool playerDetection, GameObject objectIcon)
+    {
+        // icon on condition
+        if (playerDetection && !isPanelMenuOn)
+        {
+            objectIcon.SetActive(true);
+        }
+        // icon off condition
+        if (!playerDetection || isPanelMenuOn)
+        {
+            objectIcon.SetActive(false);
+        }
+    }
+    public void Icon_Press(GameObject objectIcon)
+    {
+        objectIcon.SetActive(true);
+        isPanelMenuOn = true;
+    }
+
+    // Object Main Panel Constructors
+    public void Manual_TurnOff(GameObject objectMenu)
+    {
+        objectMenu.SetActive(false);
+        isPanelMenuOn = false;
+    }
+    public void Automatic_TurnOff(bool playerDetection, GameObject objectMenu)
+    {
+        if (!playerDetection && objectMenu.activeSelf)
+        {
+            objectMenu.SetActive(false);
+            isPanelMenuOn = false;
+        }
+    }
+
+    // Engines
     public static int EnginesOn = 0;
     void Engines_Min_Set()
     {
@@ -22,6 +52,24 @@ public class SpaceTycoon_Main_GameController : MonoBehaviour
         {
             EnginesOn = 0;
         }
+    }
+    public void Engine_On_Off(GameObject onButton, GameObject offButton, GameObject engineLight)
+    {
+        // engine on
+        if (offButton.activeSelf)
+        {
+            onButton.SetActive(true);
+            engineLight.SetActive(true);
+            EnginesOn += 1;
+        }
+        // engine off
+        if (onButton.activeSelf)
+        {
+            offButton.SetActive(true);
+            engineLight.SetActive(false);
+            EnginesOn -= 1;
+        }
+        Engines_Min_Set();
     }
 
     // craftable object storage
