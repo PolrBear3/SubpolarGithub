3c15cf24f5dd51951988659092ee849abf1b6ea0
MYSterio3 <kdw2837@gmail.com>
2022-05-30 15:11:14 +0900

    .

--- project SpaceTycoon/Assets/Scripts/Ships/snapPoint.cs
+++ project SpaceTycoon/Assets/Scripts/Ships/snapPoint.cs
@@ -2,49 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class snapPoint : MonoBehaviour
 {
-    [HideInInspector]
-    public SpriteRenderer sr;
-
-    private bool objectPlaced;
-
-    private void Awake()
-    {
-        sr = GetComponent<SpriteRenderer>();
-    }
-
-    private void Update()
-    {
-        Object_Place_Check();
-    }
-
-    void Object_Place_Check()
-    {
-        if (objectPlaced == true)
-        {
-            sr.enabled = false;
-        }
-
-        if (objectPlaced == false)
-        {
-            sr.enabled = true;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("object"))
-        {
-            objectPlaced = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("object"))
-        {
-            objectPlaced = false;
-        }
-    }
+    public bool objectPlaced;
 }
