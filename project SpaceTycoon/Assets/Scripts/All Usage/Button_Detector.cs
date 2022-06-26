e5e3af8372ba25c8503ec67b50ad0e33e40ac314
MYSterio3 <kdw2837@gmail.com>
2022-06-26 17:39:46 +0900

    .

--- project SpaceTycoon/Assets/Scripts/All Usage/Button_Detector.cs
+++ project SpaceTycoon/Assets/Scripts/All Usage/Button_Detector.cs
@@ -16,4 +16,11 @@ public class Button_Detector : MonoBehaviour
     {
         buttonPressed = false;
     }
+
+    // how to use Button_Detector
+    // 1. attach this script to button
+    // 2. set On Click Runtime to Button_Press()
+    // 3. at other button controller script, use it as bool ex) if (gameButton.buttonPressed == true)
+    // 4. put all the functions you want for when button is pressed
+    // 5. after all the functions are made, set the button back to false ex) gameButton.Set_Backto_UnPressed();
 }
