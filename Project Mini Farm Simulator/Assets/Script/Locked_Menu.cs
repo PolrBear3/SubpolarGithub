2071bf0e15292d16dbcf7cda5f6cdf4a64c40f13
MYSterio3 <kdw2837@gmail.com>
2022-07-04 18:38:38 +0900

    .

--- Project Mini Farm Simulator/Assets/Script/Locked_Menu.cs
+++ Project Mini Farm Simulator/Assets/Script/Locked_Menu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Locked_Menu : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class Locked_Menu : MonoBehaviour
     RectTransform rectTransform;
     public LeanTweenType tweenType;
 
+    public Text tilePrice;
+
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -16,11 +19,27 @@ public class Locked_Menu : MonoBehaviour
 
     public void Open()
     {
+        tilePrice.text = controller.farmTiles[controller.openedTileNum].tilePrice + "$".ToString();
         LeanTween.move(rectTransform, new Vector2(0f, 104.85f), 0.75f).setEase(tweenType);
     }
     public void Close()
     {
         controller.Reset_All_Tile_Highlights();
         LeanTween.move(rectTransform, new Vector2(0f, -125f), 0.75f).setEase(tweenType);
+    }
+
+    public void Buy_Tile()
+    {
+        var selectedTile = controller.farmTiles[controller.openedTileNum];
+
+        if (controller.money >= selectedTile.tilePrice)
+        {
+            selectedTile.Unlock_Tile();
+            Close();
+        }
+        else
+        {
+            Debug.Log("Not enough money!");
+        }
     }
 }
