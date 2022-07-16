83dffe4acea00c1a5b2e6d676983754ac3d2ec2c
MYSterio3 <kdw2837@gmail.com>
2022-07-17 03:06:17 +0900

    .

---
+++ Project Mini Farm Simulator/Assets/Script/Page_Controller.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Page_Controller : MonoBehaviour
+{
+    public GameObject[] pages;
+    public Text currentPageNumText;
+
+    private int _currentPageNum;
+    public int currentPageNum => _currentPageNum;
+
+    private void Start()
+    {
+        Restart_Deactivate_AllPages();
+        _currentPageNum = 1;
+        Activate_Page();
+        PageNum_Text_Update();
+    }
+
+    // private functions
+    private void Restart_Deactivate_AllPages()
+    {
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(false);
+        }
+    }
+
+    private void Activate_Page()
+    {
+        int scanNum = 0;
+        for (int i = 0; i < pages.Length; i++)
+        {
+            scanNum++;
+            if (scanNum == currentPageNum)
+            {
+                pages[i].SetActive(true);
+                break;
+            }
+        }
+    }
+
+    private void Check_PageNum_StartEnd()
+    {
+        // check start
+        if (currentPageNum < 1)
+        {
+            _currentPageNum = pages.Length;
+        }
+        // check end
+        else if (currentPageNum > pages.Length)
+        {
+            _currentPageNum = 1;
+        }
+    }
+
+    private void PageNum_Text_Update()
+    {
+        currentPageNumText.text = currentPageNum + " / " + pages.Length.ToString();
+    }
+
+    // public functions
+    public void NextPage()
+    {
+        _currentPageNum++;
+        Restart_Deactivate_AllPages();
+        Check_PageNum_StartEnd();
+        Activate_Page();
+        PageNum_Text_Update();
+    }
+
+    public void BackPage()
+    {
+        _currentPageNum--;
+        Restart_Deactivate_AllPages();
+        Check_PageNum_StartEnd();
+        Activate_Page();
+        PageNum_Text_Update();
+    }
+}
