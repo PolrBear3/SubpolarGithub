4a9007357d20152ef62a7bc0cbeba72a6eceacc2
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2022-09-26 20:28:48 +0900

    .

--- Project Games/Project Mini Farm Simulator/Assets/Script/Buff/Buff_Functions.cs
+++ Project Games/Project Mini Farm Simulator/Assets/Script/Buff/Buff_Functions.cs
@@ -8,14 +8,29 @@ public class Buff_Functions : MonoBehaviour
     
     public void Cloudy_Stun_Shield(FarmTile farmtile)
     {
+        int removeAtNum = 0;
+
         // if the weather is cloudy
         if (functionController.controller.eventSystem.currentWeather.weatherID == 1)
         {
-            // give back 1 heatlh
-
+            // give back 1 day passed points
+            farmtile.tileSeedStatus.dayPassed += 1;
             // get rid of cloudy stun icon
-
+            farmtile.statusIconIndicator.UnAssign_Status(StatusType.cloudyStunned);
             // remove from buff list
+            for (int i = 0; i < farmtile.currentBuffs.Count; i++)
+            {
+                if (farmtile.currentBuffs[i].buffID == 0)
+                {
+                    farmtile.currentBuffs.RemoveAt(removeAtNum);
+                    break;
+                }
+
+                removeAtNum ++;
+            }
+            // update tile
+            farmtile.TileSprite_Update_Check();
+            functionController.controller.plantedMenu.Seed_Information_Update();
         }
     }
 }
