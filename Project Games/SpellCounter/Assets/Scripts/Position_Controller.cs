7c49251245d0a458ad9d48b391ebd7eaab5af5f6
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2022-11-03 18:32:21 +0900

    .

---
+++ Project Games/SpellCounter/Assets/Scripts/Position_Controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Position_Controller_Data
+{
+
+}
+
+[System.Serializable]
+public class Position_Controller_UI
+{
+
+}
+
+public class Position_Controller : MonoBehaviour
+{
+    public Controller controller;
+    public Position_Controller_Data data;
+    public Position_Controller_UI ui;
+}
---
+++ Project Games/SpellCounter/Assets/Scripts/Spell_ScrObj.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "New Spell")]
+public class Spell_ScrObj : ScriptableObject
+{
+    public Sprite sprite;
+
+    public float defaultCoolTime;
+    public float bootsCoolTime;
+    public float runeCoolTime;
+    public float bothCoolTime;
+}
