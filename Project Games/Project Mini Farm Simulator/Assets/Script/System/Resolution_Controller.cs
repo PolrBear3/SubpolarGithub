4b03fe21a943c719b1a76efdce3df78433dd1bde
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2022-11-09 16:40:29 +0900

    .

--- Project Games/Project Mini Farm Simulator/Assets/Script/System/Resolution_Controller.cs
+++ Project Games/Project Mini Farm Simulator/Assets/Script/System/Resolution_Controller.cs
@@ -2,7 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct Available_Resolution
+{
+    public int width;
+    public int height;
+}
+
+[System.Serializable]
+public class Resolution_Controller_Data
+{
+    public bool isFullScreen;
+}
+
 public class Resolution_Controller : MonoBehaviour
 {
+    public Resolution_Controller_Data data;
+    public Available_Resolution[] resolutions;
 
+    public void FullScreen_OnOff()
+    {
+        if (!data.isFullScreen)
+        {
+            data.isFullScreen = true;
+            Screen.fullScreen = data.isFullScreen;
+        }
+        else
+        {
+            data.isFullScreen = false;
+            Screen.fullScreen = data.isFullScreen;
+        }
+    }
+    public void Change_Resolution(int arrayNum)
+    {
+        if (data.isFullScreen)
+        {
+            Screen.SetResolution(resolutions[arrayNum].width, resolutions[arrayNum].height, true);
+        }
+        else
+        {
+            Screen.SetResolution(resolutions[arrayNum].width, resolutions[arrayNum].height, false);
+        }
+    }
 }
