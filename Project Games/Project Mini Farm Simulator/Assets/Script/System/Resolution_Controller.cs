60035cd69135ae0eb115db644202999a10cb50e1
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2022-11-16 23:48:03 +0900

    Project Mini Farm Simulator
    update
    - new apply resolution panel with timer available

--- Project Games/Project Mini Farm Simulator/Assets/Script/System/Resolution_Controller.cs
+++ Project Games/Project Mini Farm Simulator/Assets/Script/System/Resolution_Controller.cs
@@ -14,7 +14,17 @@ public struct Available_Resolution
 public class Resolution_Controller_Data
 {
     public bool isFullScreen = false;
-    public int currentWidth, currentHeight;
+    public int currentResArrayNum;
+
+    [HideInInspector]
+    public int preResArrayNum;
+    [HideInInspector]
+    public bool isPreFullScreen = false;
+    [HideInInspector]
+    public bool countApplyResTime = false;
+    [HideInInspector]
+    public float currentApplyResTime;
+    public float maxApplyResTime;
 }
 
 [System.Serializable]
@@ -22,6 +32,9 @@ public class Resolution_Controller_UI
 {
     public Text currentResolution;
     public Text currentScreenMode;
+
+    public GameObject appyResPanel;
+    public Text applyResTimeText;
 }
 
 public class Resolution_Controller : MonoBehaviour
@@ -33,45 +46,126 @@ public class Resolution_Controller : MonoBehaviour
 
     private void Start()
     {
-        Change_Resolution(1);
+        Load_Resolution_and_ScreenMode();
+    }
+    private void Update()
+    {
+        Resolution_Panel_Timer();
     }
 
     private void Update_Current_Resolution_Text(int arrayNum)
     {
         ui.currentResolution.text = resolutions[arrayNum].width + " * " + resolutions[arrayNum].height.ToString();
     }
+    private void Update_Current_ScreenMode_Text(bool isFullScreen)
+    {
+        if (isFullScreen)
+        {
+            ui.currentScreenMode.text = "Full".ToString(); ;
+        }
+        else
+        {
+            ui.currentScreenMode.text = "Window".ToString(); ;
+        }
+    }
 
+    private void Load_Resolution_and_ScreenMode()
+    {
+        // default resolution for test (function not complete)
+        data.currentResArrayNum = 2;
+        var x = data.currentResArrayNum;
+        Screen.SetResolution(resolutions[x].width, resolutions[x].height, data.isFullScreen);
+
+        Update_Current_Resolution_Text(x);
+    }
+    
     public void Change_ScreenMode(bool on)
     {
+        Resolution_Panel_Popup();
+
         if (on == true)
         {
             data.isFullScreen = true;
-            Screen.SetResolution(data.currentWidth, data.currentHeight, true);
-            ui.currentScreenMode.text = "Full".ToString();
+            Screen.SetResolution(data.currentResArrayNum, data.currentResArrayNum, true);
         }
-        
-        if (on == false)
+        else
         {
             data.isFullScreen = false;
-            Screen.SetResolution(data.currentWidth, data.currentHeight, false);
-            ui.currentScreenMode.text = "Window".ToString();
+            Screen.SetResolution(data.currentResArrayNum, data.currentResArrayNum, false);
         }
+
+        Update_Current_ScreenMode_Text(on);
     }
     public void Change_Resolution(int arrayNum)
     {
+        Resolution_Panel_Popup();
+
         if (data.isFullScreen)
         {
             Screen.SetResolution(resolutions[arrayNum].width, resolutions[arrayNum].height, true);
-            data.currentWidth = resolutions[arrayNum].width;
-            data.currentHeight = resolutions[arrayNum].height;
         }
         else
         {
             Screen.SetResolution(resolutions[arrayNum].width, resolutions[arrayNum].height, false);
-            data.currentWidth = resolutions[arrayNum].width;
-            data.currentHeight = resolutions[arrayNum].height;
         }
+        data.currentResArrayNum = arrayNum;
 
         Update_Current_Resolution_Text(arrayNum);
+    }
+
+    private void Resolution_Panel_Timer()
+    {
+        // if apply res panel is on, reset timer and start the timer
+        if (data.countApplyResTime)
+        {
+            // time text update
+            ui.applyResTimeText.text = data.currentApplyResTime.ToString("f0");
+            
+            // count delta time
+            data.currentApplyResTime -= Time.deltaTime;
+            // if timer reaches 0, Reset_Back_Resolution()
+            if (data.currentApplyResTime <= 0)
+            {
+                data.countApplyResTime = false;
+                Reset_Back_Resolution();
+            }
+        }
+    }
+    private void Resolution_Panel_Popup()
+    {
+        // save pre apply resolution
+        data.preResArrayNum = data.currentResArrayNum;
+        data.isPreFullScreen = data.isFullScreen;
+        // apply res panel shows up
+        ui.appyResPanel.SetActive(true);
+        // set timer
+        data.currentApplyResTime = data.maxApplyResTime;
+        // start timer
+        data.countApplyResTime = true;
+        // all the buttons on the back, unfunctional while it is showing
+    }
+    
+    public void Apply_Resolution()
+    {
+        // deactivate apply res panel
+        ui.appyResPanel.SetActive(false);
+        // stop timer
+        data.countApplyResTime = false;
+        // all the buttons on the back, functional
+    }
+    public void Reset_Back_Resolution()
+    {
+        // deactivate apply res panel
+        ui.appyResPanel.SetActive(false);
+        // stop timer
+        data.countApplyResTime = false;
+        // set back to the past resolution, update data and ui
+        var x = data.preResArrayNum;
+        Screen.SetResolution(resolutions[x].width, resolutions[x].height, data.isPreFullScreen);
+        data.currentResArrayNum = x;
+        data.isFullScreen = data.isPreFullScreen;
+        Update_Current_Resolution_Text(x);
+        Update_Current_ScreenMode_Text(data.isFullScreen);
+        // all the buttons on the back, functional
     }
 }
