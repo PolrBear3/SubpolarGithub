2eb232fa09d900994b7b127df5770c60535c4bff
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2022-12-01 04:14:07 +0900

    .

---
+++ Project Games/SandBox/Assets/Custom Systems/Gacha System/Scripts/Gacha_System.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Gacha_System_Data
+{
+    public Gacha_System_Item[] items;
+}
+
+[System.Serializable]
+public class Gacha_System_UI
+{
+    public Image[] itemImages;
+    public Text[] itemAmountTexts;
+    public Image cropImage;
+}
+
+[System.Serializable]
+public class Gacha_System_Item
+{
+    public Item_Info item;
+    public float percentage;
+    public int currentAmount;
+}
+
+public class Gacha_System : MonoBehaviour
+{
+    public Gacha_System_Data data;
+    public Gacha_System_UI ui;
+
+    private void Start()
+    {
+        Update_Item_UI();
+    }
+
+    private void Update_Item_UI()
+    {
+        for (int i = 0; i < data.items.Length; i++)
+        {
+            ui.itemImages[i].sprite = data.items[i].item.itemSprite;
+            ui.itemAmountTexts[i].text = data.items[i].currentAmount.ToString();
+        }
+    }
+
+    private bool Percentage_Setter(float percentage)
+    {
+        var value = (100 - percentage) * 0.01f;
+
+        if (Random.value > value)
+        {
+            return true;
+        }
+        else return false;
+    }
+    public void Gacha_Random_Item()
+    {
+        bool gachaCheckComplete = false;
+
+        while (!gachaCheckComplete)
+        {
+            int randomNum = Random.Range(0, data.items.Length);
+            var randomItem = data.items[randomNum];
+
+            if (Percentage_Setter(randomItem.percentage))
+            {
+                gachaCheckComplete = true;
+
+                ui.cropImage.sprite = randomItem.item.itemSprite;
+                // counting amount
+                // update amount ui
+            }
+        }
+    }
+}
