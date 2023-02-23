e798f38b84dc0a421ea2a5de648fbdb12bc233a3
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2023-02-23 22:04:47 +0900

    .

--- Project Games/Project Token Dungeon/Assets/Scripts/Actions/Action_Move.cs
+++ Project Games/Project Token Dungeon/Assets/Scripts/Actions/Action_Move.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Action_Move : MonoBehaviour
+public class Action_Move : MonoBehaviour, IAction
 {
     public Action_Controller controller;
+
+    public void Action()
+    {
+        Debug.Log("check");
+    }
 }
--- Project Games/Project Token Dungeon/Assets/Scripts/Controllers/Action_Controller.cs
+++ Project Games/Project Token Dungeon/Assets/Scripts/Controllers/Action_Controller.cs
@@ -2,16 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IAction
-{
-    void Action();
-}
-
-public enum aimDirection
-{
-    top, right, bottom, left
-}
-
 public class Action_Controller : MonoBehaviour
 {
     public Player_Piece playerPiece;
