6f7109e0e431f0c1efa2c5547e94a42463e84c41
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2023-03-28 22:52:37 +0900

    Project Mini Farm Simulator
    update
    - stormy and snowy sounds added
    - options menu bottom panel changed to option menu open close button

--- Project Games/Project Mini Farm Simulator/Assets/Script/Menus/Option Menu/Options_Menu.cs
+++ Project Games/Project Mini Farm Simulator/Assets/Script/Menus/Option Menu/Options_Menu.cs
@@ -12,23 +12,7 @@ public class Options_Menu_Data
 [System.Serializable]
 public class Options_Menu_UI
 {
-    public RectTransform optionsPanel, bottomPanel;
-    public GameObject[] menuButtonIcons;
-    
-    public Sprite[] buttonImages;
-    public Options_Menu_TopButton[] topButtons;
-
-    public GameObject[] settingsPages;
-    public GameObject[] bottomSettingsPages;
-}
-
-[System.Serializable]
-public class Options_Menu_TopButton
-{
-    public Button button;
-    public Image buttonImage;
-    public RectTransform buttonIcon;
-    public Image buttonIconImage;
+    public RectTransform optionsPanel;
 }
 
 public class Options_Menu : MonoBehaviour
@@ -40,11 +24,6 @@ public class Options_Menu : MonoBehaviour
 
     public Options_Menu_Data data;
     public Options_Menu_UI ui;
-
-    private void Start()
-    {
-        BottomPanel_Center_Position();
-    }
 
     public void Button_Shield(bool activate)
     {
@@ -65,14 +44,8 @@ public class Options_Menu : MonoBehaviour
 
         Button_Shield(false);
 
-        // default settings page
-        Open_GameSettings();
-
         data.menuOn = true;
         LeanTween.move(ui.optionsPanel, new Vector2(0f, 62.50972f), 0.75f).setEase(tweenType);
-        LeanTween.move(ui.bottomPanel, new Vector2(0f, 104.85f), 0.75f).setEase(tweenType);
-        ui.menuButtonIcons[0].SetActive(false);
-        ui.menuButtonIcons[1].SetActive(true);
 
         // button shield on for nextday button
         controller.defaultMenu.menuUI.nextDayButton.enabled = false;
@@ -86,9 +59,6 @@ public class Options_Menu : MonoBehaviour
 
         data.menuOn = false;
         LeanTween.move(ui.optionsPanel, new Vector2(-500f, 62.50972f), 0.75f).setEase(tweenType);
-        LeanTween.move(ui.bottomPanel, new Vector2(0f, -125f), 0.75f).setEase(tweenType);
-        ui.menuButtonIcons[0].SetActive(true);
-        ui.menuButtonIcons[1].SetActive(false);
 
         // button shield on for nextday button
         controller.defaultMenu.menuUI.nextDayButton.enabled = true;
@@ -100,65 +70,5 @@ public class Options_Menu : MonoBehaviour
     {
         if (!data.menuOn) { Open(); }
         else { Close(); }
-    }
-
-    // bottom options panel
-    private void BottomPanel_Center_Position()
-    {
-        ui.bottomPanel.anchoredPosition = new Vector2(0f, -125f);
-    }
-
-    // top main buttons
-    private void Unpress_All_TopButtons()
-    {
-        for (int i = 0; i < ui.topButtons.Length; i++)
-        {
-            ui.topButtons[i].button.enabled = true;
-            ui.topButtons[i].buttonImage.sprite = ui.buttonImages[0];
-            ui.topButtons[i].buttonIcon.anchoredPosition = new Vector2(0f, 3.2f);
-        }
-    }
-    private void Press_TopButton(int buttonNum)
-    {
-        Unpress_All_TopButtons();
-        ui.topButtons[buttonNum].button.enabled = false;
-        ui.topButtons[buttonNum].buttonImage.sprite = ui.buttonImages[1];
-        ui.topButtons[buttonNum].buttonIcon.anchoredPosition = new Vector2(0f, -3.2f);
-    }
-
-    private void Reset_All_MenuPages()
-    {
-        for (int i = 0; i < ui.settingsPages.Length; i++)
-        {
-            ui.settingsPages[i].SetActive(false);
-            ui.bottomSettingsPages[i].SetActive(false);
-        }
-    }
-    private void Open_MenuPage(int menuPageNum)
-    {
-        Reset_All_MenuPages();
-        ui.settingsPages[menuPageNum].SetActive(true);
-        ui.bottomSettingsPages[menuPageNum].SetActive(true);
-    }
-
-    public void Open_GameSettings()
-    {
-        Press_TopButton(0);
-        Open_MenuPage(0);
-    }
-    public void Open_HowtoPlay()
-    {
-        Press_TopButton(1);
-        Open_MenuPage(1);
-    }
-    public void Open_AllSeedBuffInfo()
-    {
-        Press_TopButton(2);
-        Open_MenuPage(2);
-    }
-    public void Open_AllStatusInfo()
-    {
-        Press_TopButton(3);
-        Open_MenuPage(3);
     }
 }
