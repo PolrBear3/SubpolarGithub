76a799d03fe4e51f5cbe4a46f90fe307638985b8
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2023-04-07 16:02:08 +0900

    .

--- Project Games/project BitCraft/Assets/Scripts/Basic_Tile.cs
+++ Project Games/project BitCraft/Assets/Scripts/Basic_Tile.cs
@@ -8,10 +8,17 @@ public class Basic_Tile : MonoBehaviour
     private SpriteRenderer sr;
 
     [Header("Data")]
-    [SerializeField] private int tileNum;
+    [SerializeField] private int tileNumX;
+    [SerializeField] private int tileNumY;
 
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void Set_Data(int x, int y)
+    {
+        tileNumX = x;
+        tileNumY = y;
     }
 }
