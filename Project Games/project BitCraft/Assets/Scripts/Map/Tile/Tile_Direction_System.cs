542c54f5a28128beb37cd4af9e58adef03a38f2a
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2023-05-04 12:17:24 +0900

    .

--- Project Games/project BitCraft/Assets/Scripts/Map/Tile/Tile_Direction_System.cs
+++ Project Games/project BitCraft/Assets/Scripts/Map/Tile/Tile_Direction_System.cs
@@ -9,7 +9,6 @@ public class Tile_Direction_System : MonoBehaviour
     [SerializeField] private GameObject[] _directions;
 
     [SerializeField] private List<Tile_Controller> crossTileEx = new List<Tile_Controller>();
-    [SerializeField] private List<Map_Controller> surroundingMapsEx = new List<Map_Controller>();
 
     private void Awake()
     {
@@ -42,37 +41,34 @@ public class Tile_Direction_System : MonoBehaviour
         Map_Controller currentMap = mapController.currentMap;
 
         List<Tile_Controller> crossTiles = new List<Tile_Controller>();
-        List<Map_Controller> surroundingMaps = new List<Map_Controller>();
 
         // top
         crossTiles.Add(mapController.Get_Tile(_tileController.rowNum, _tileController.columnNum - 1));
-        surroundingMaps.Add(mapController.Get_Map(currentMap.positionX, currentMap.positionY++));
-        if (crossTiles[0] == null && surroundingMaps[0] != null) crossTiles[0] = surroundingMaps[0].Get_Tile(currentMap.positionX, surroundingMaps[0].mapSize - 1);
+        Map_Controller topMap = mapController.Get_Map(currentMap.positionX, currentMap.positionY++);
+        if (crossTiles[0] == null && topMap != null) crossTiles[0] = topMap.Get_Tile(currentMap.positionX, currentMap.mapSize - 1);
 
         // right
         crossTiles.Add(mapController.Get_Tile(_tileController.rowNum + 1, _tileController.columnNum));
-        surroundingMaps.Add(mapController.Get_Map(currentMap.positionX++, currentMap.positionY));
-        if (crossTiles[1] == null && surroundingMaps[1] != null) crossTiles[1] = surroundingMaps[1].Get_Tile(0, currentMap.positionY);
+        Map_Controller rightMap = mapController.Get_Map(currentMap.positionX++, currentMap.positionY);
+        if (crossTiles[1] == null && rightMap != null) crossTiles[1] = rightMap.Get_Tile(0, currentMap.positionY);
 
         // bottom
         crossTiles.Add(mapController.Get_Tile(_tileController.rowNum , _tileController.columnNum + 1));
-        surroundingMaps.Add(mapController.Get_Map(currentMap.positionX, currentMap.positionY--));
-        if (crossTiles[2] == null && surroundingMaps[2] != null) crossTiles[2] = surroundingMaps[2].Get_Tile(currentMap.positionX, 0);
+        Map_Controller bottomMap = mapController.Get_Map(currentMap.positionX, currentMap.positionY--);
+        if (crossTiles[2] == null && bottomMap != null) crossTiles[2] = bottomMap.Get_Tile(currentMap.positionX, 0);
 
         // left
         crossTiles.Add(mapController.Get_Tile(_tileController.rowNum - 1, _tileController.columnNum));
-        surroundingMaps.Add(mapController.Get_Map(currentMap.positionX--, currentMap.positionY));
-        if (crossTiles[3] == null && surroundingMaps[3] != null) crossTiles[3] = surroundingMaps[3].Get_Tile(surroundingMaps[3].mapSize - 1, currentMap.positionY);
+        Map_Controller leftMap = mapController.Get_Map(currentMap.positionX--, currentMap.positionY);
+        if (crossTiles[3] == null && leftMap != null) crossTiles[3] = leftMap.Get_Tile(currentMap.mapSize - 1, currentMap.positionY);
 
         crossTileEx = crossTiles;
-        surroundingMapsEx = surroundingMaps;
 
         for (int i = 0; i < crossTiles.Count; i++)
         {
             if (crossTiles[i] != null) continue;
-            if (crossTiles[i].Is_Prefab_Type(Prefab_Type.placeable)) continue;
-
             _directions[i].SetActive(true);
+            // if (crossTiles[i].Is_Prefab_Type(Prefab_Type.placeable)) continue;
         }
     }
 }
