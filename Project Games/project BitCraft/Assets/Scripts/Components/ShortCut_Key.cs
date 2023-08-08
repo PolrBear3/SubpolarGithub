455698b2d4134b8f35958ec1d9ea7b635c97d1bc
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2023-08-08 18:34:46 +0900

    .

---
+++ Project Games/project BitCraft/Assets/Scripts/Components/ShortCut_Key.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortCut_Key : MonoBehaviour
+{
+    [SerializeField] private GameObject _keyText;
+}
--- Project Games/project BitCraft/Assets/Scripts/Controller/Time/Time_Controller.cs
+++ Project Games/project BitCraft/Assets/Scripts/Controller/Time/Time_Controller.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Time_Controller : MonoBehaviour
 {
     [Header("Inspector Set")]
     [SerializeField] private Game_Controller _gameController;
 
-    [SerializeField] private List<Time_Slot> _timeSlot = new List<Time_Slot>();
-    public List<Time_Slot> timeSlot { get => _timeSlot; set => _timeSlot = value; }
-
-    private int _currentTimeCount;
-    public int currentTimeCount { get => _currentTimeCount; set => _currentTimeCount = value; }
+    private int _currentTime;
+    public int currentTime { get => _currentTime; set => _currentTime = value; }
 }
