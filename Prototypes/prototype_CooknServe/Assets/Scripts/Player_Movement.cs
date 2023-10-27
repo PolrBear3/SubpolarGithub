14a4371de3a2d6db9123ab7e75aa5954cc20d7f7
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2023-10-27 22:22:50 +0900

    .

--- Prototypes/prototype_CooknServe/Assets/Scripts/Player_Movement.cs
+++ Prototypes/prototype_CooknServe/Assets/Scripts/Player_Movement.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_Movement : MonoBehaviour
 {
     [SerializeField] private Player_Controller _playerController;
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        
+    }
 }
