e486d4f7970b10cdc47341f9c2fd72ca4dc5a37f
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2023-11-24 13:14:30 +0900

    .

--- Prototypes/prototype_CooknServe/Assets/Scripts/Station/Oven.cs
+++ Prototypes/prototype_CooknServe/Assets/Scripts/Station/Oven.cs
@@ -7,8 +7,12 @@ public class Oven : MonoBehaviour, IInteractable
     private SpriteRenderer _sr;
 
     private Game_Controller _gameController;
+    private Player_Controller _playerController;
 
     private Food _currentFood;
+
+    [SerializeField] private Sprite _activeSprite;
+    [SerializeField] private Sprite _inactiveSprite;
 
     // UnityEngine
     private void Awake()
@@ -18,15 +22,56 @@ public class Oven : MonoBehaviour, IInteractable
         if (gameObject.TryGetComponent(out SpriteRenderer sr)) { _sr = sr; }
     }
 
+    // OnTrigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.TryGetComponent(out Player_Controller playerController)) return;
+        _playerController = playerController;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.TryGetComponent(out Player_Controller playerController)) return;
+        _playerController = null;
+    }
+
     // IInteractable
     public void Interact()
     {
+        if (_playerController == null) return;
+        if (!_playerController.playerInteraction.Is_Closest_Interactable(gameObject)) return;
 
+        Swap_Food();
+        Heat_Food();
     }
 
     // Custom
-    private void Swap()
+    private void Swap_Food()
     {
+        Player_Interaction player = _playerController.playerInteraction;
+        Food playerFood = player.currentFood;
 
+        player.Set_CurrentFood(_currentFood);
+        _currentFood = playerFood;
+
+        if (_currentFood == null)
+        {
+            // empty current food here !
+            return;
+        }
+
+        // update current food here !
+    }
+    private void Heat_Food()
+    {
+        // active
+        if (_currentFood != null)
+        {
+            _sr.sprite = _activeSprite;
+        }
+        // inactive
+        else
+        {
+            _sr.sprite = _inactiveSprite;
+        }
     }
 }
