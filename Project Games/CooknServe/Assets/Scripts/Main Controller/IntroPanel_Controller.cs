cfd8c7bfb629ac55fcbc2bf57c6ae857d916f1a8
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2024-01-03 15:55:12 +0900

    .

---
+++ Project Games/CooknServe/Assets/Scripts/Main Controller/IntroPanel_Controller.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class IntroPanel_Controller : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _pages = new();
+
+    public void Page_Control()
+    {
+
+    }
+}
