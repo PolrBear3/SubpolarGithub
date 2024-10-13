a100f73e05038f8be69f7f746e6e7a2006e59206
PolrBear <91929030+PolrBear3@users.noreply.github.com>
2024-10-13 13:15:09 +0900

    TastyRoad_main 0.26
    - removed station interaction and added action bubble interactable component

--- Projects/TastyRoad/Assets/Scripts/Stations/Scrap.cs
+++ Projects/TastyRoad/Assets/Scripts/Stations/Scrap.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Scrap : MonoBehaviour, IInteractable
+public class Scrap : MonoBehaviour
 {
     private SpriteRenderer _sr;
 
-    private Station_Controller _stationController;
+    [Header("")]
+    [SerializeField] private Station_Controller _stationController;
     public Station_Controller stationController => _stationController;
 
-    [SerializeField] private Action_Bubble _actionBubble;
+    [SerializeField] private ActionBubble_Interactable _interactable;
 
     [Header("")]
     [SerializeField] private List<Sprite> _scrapSprites = new();
-
 
 
     // UnityEngine
@@ -29,60 +29,10 @@ public class Scrap : MonoBehaviour, IInteractable
     }
 
 
-
-    // OnTrigger
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (_stationController.movement.enabled) return;
-        if (!collision.TryGetComponent(out Player_Controller player)) return;
-
-        UnInteract();
-    }
-
-
-
-    // IInteractable
-    public void Interact()
-    {
-        if (_actionBubble.bubbleOn)
-        {
-            UnInteract();
-            return;
-        }
-
-        _actionBubble.Toggle(true);
-
-        _stationController.PlayerInput_Activation(true);
-        _stationController.Action1_Event += Collect_toVehicle;
-    }
-
-    public void UnInteract()
-    {
-        _actionBubble.Toggle(false);
-
-        _stationController.PlayerInput_Activation(false);
-        _stationController.Action1_Event -= Collect_toVehicle;
-    }
-
-
-
     //
     private void Set_RandomSprite()
     {
         int randArrayNum = Random.Range(0, _scrapSprites.Count);
         _sr.sprite = _scrapSprites[randArrayNum];
-    }
-
-    private void Collect_toVehicle()
-    {
-        StationMenu_Controller menu = _stationController.mainController.currentVehicle.menu.stationMenu;
-
-        // check if station slots are all full
-
-        // add this station to vehicle station menu
-        menu.Add_StationItem(_stationController.stationScrObj, 1);
-
-        // destroy
-        _stationController.Destroy_Station();
     }
 }
